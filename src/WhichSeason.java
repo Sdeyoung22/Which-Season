42f57531a69377eca8f98a70880b69e3ae27a268
mooin <mooin@LAPTOP-NF7TBUIA.fios-router.home>
2018-11-13 15:24:15 -0500

    Added results

--- src/WhichSeason.java
+++ src/WhichSeason.java
@@ -14,12 +14,27 @@ public class WhichSeason
 		System.out.println("WHICH SEASON?");
 		System.out.print("Press enter to start");
 		input.nextLine();
-		System.out.print("Enter the numerical value of a month:  ");
+		System.out.print("Enter the numerical value of your birth month:  ");
 		int mnth = input.nextInt();
 		
 		if (mnth < 1 || mnth > 12)
 			System.out.print("INVALID MONTH");
-		else if (mnth >= 3 && mnth <= 5);
+		else if (mnth >= 3 && mnth <= 5)
+		{
+			System.out.print("You were born in Spring!");
+		}
+		else if (mnth >= 6 && mnth <= 8)
+		{
+			System.out.print("You were born in Summer!");
+		}
+		else if (mnth >= 9 && mnth <= 11)
+		{
+			System.out.print("You were born in Autumn!");
+		}
+		else if (mnth == 12 || mnth <= 2)
+		{
+			System.out.print("You were born in Winter!");
+		}
 		
 
 	}
